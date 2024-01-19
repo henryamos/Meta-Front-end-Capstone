4bdf67e768186aaec14b6c7876d553594c81f28d
Henry Amos <amoshenry95@gmail.com>
2024-01-19 11:40:49 +0000

    Bokkings and Confirmed Bookings Booking Form Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import './App.css'
 import Nav from './components/Nav'
-import Header from './components/Header'
+import Main from './components/Main';
 
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
     <>
     <Router>
      <Nav/>
-     <Header/>
-     </Router>
+    <Main/>
+    </Router>
      </>
   )
 }
